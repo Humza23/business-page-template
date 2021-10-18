85343ed308e1185e613dec04dbc7fadffd3b2f48
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-10-17 20:01:58 -0400

    wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = ({ nav }) => {
     return (
         <nav className={nav ? 'nav active' : 'nav'}>
-            <a href="#home"><img src="./images/logo.png" alt="logo" className={nav ? 'logo active' : 'logo'}/></a>
+            <a href="/business-page-template/#home"><img src="./images/logo.png" alt="logo" className={nav ? 'logo active' : 'logo'}/></a>
             <div>
-                <a href="#gallery">Gallery</a>
-                <a href="#gallery2">Gallery 2</a>
-                <a href="#footer">Contact</a>
+                <a href="/business-page-template/#gallery">Gallery</a>
+                <a href="/business-page-template/#gallery2">Gallery 2</a>
+                <a href="/business-page-template/#footer">Contact</a>
             </div>
         </nav>
     )
